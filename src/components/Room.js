5c4598834bbaad03bf094d9cc2ed115b831bbfb5
Brian Farnsworth <farnswor@adobe.com>
2017-05-03 23:52:45 -0600

    adding style for join room section

--- src/components/Room.js
+++ src/components/Room.js
@@ -17,9 +17,9 @@ class Room extends React.Component {
    get configSection() {
       if(this.state.roomOption === 'host') {
          return (
-            <div className="host-section">
-               <div className="host-section-name-input">Name: <input className="bf-input" type="text"/></div>
-               <div className="host-section-button-row">
+            <div className="room-config-section host-config-section">
+               <div className="bf-input-container">Name: <input className="bf-input" type="text"/></div>
+               <div className="room-section-button-row">
                   <button className="bf-button game-config-button-horizontal">Create Room</button>
                   <button className="bf-button game-config-button-horizontal" onClick={() => {this.setState({roomOption: 'start'})}}>Back</button>
                </div>
@@ -27,16 +27,18 @@ class Room extends React.Component {
          );
       } else if(this.state.roomOption === 'join') {
          return (
-            <div>
-               name: <input type="text"/>
-               code: <input type="text"/>
-               <button>Join</button>
-               <button onClick={() => {this.setState({roomOption: 'start'})}}>Back</button>
+            <div className="room-config-section">
+               <div className="bf-input-container">Name: <input className="bf-input" type="text"/></div>
+               <div className="bf-input-container">Code: <input className="bf-input" type="text"/></div>
+               <div className="room-section-button-row">
+                  <button className="bf-button game-config-button-horizontal">Join</button>
+                  <button className="bf-button game-config-button-horizontal" onClick={() => {this.setState({roomOption: 'start'})}}>Back</button>
+               </div>
             </div>
          );
       } else if(this.state.roomOption === 'start') {
          return (
-            <div className="join-or-host-section">
+            <div className="room-config-section">
                <button className="game-config-button-vertical bf-button" onClick={() => {this.setState({roomOption: 'host'})}}>Host Game</button>
                <button className="game-config-button-vertical bf-button" onClick={() => {this.setState({roomOption: 'join'})}}>Join Game</button>
             </div>
