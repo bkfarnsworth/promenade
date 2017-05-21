a692f08230ddfeb80de7767182ef4d27f52caf74
Brian Farnsworth <farnswor@adobe.com>
2017-05-21 00:11:38 -0600

    Changing spaces to tabs

--- src/components/App.js
+++ src/components/App.js
@@ -15,79 +15,76 @@ import './App.css';
 
 class Game {
 
-  constructor(opts={}) {
+	constructor(opts={}) {
 
-    _.defaults(opts, {
-      name: 'Game Name',
-      getGameComponent: () => <div>Game Component</div>,
-      initialState: 'room'
-    });
+		_.defaults(opts, {
+			name: 'Game Name',
+			getGameComponent: () => <div>Game Component</div>,
+			initialState: 'room'
+		});
 
-    this.name = opts.name;
-    this.getGameComponent = opts.getGameComponent;
-    this.initialState = opts.initialState;
-  }
+		this.name = opts.name;
+		this.getGameComponent = opts.getGameComponent;
+		this.initialState = opts.initialState;
+	}
 
-  getPath() {
-    return '/' + this.name;
-  }
+	getPath() {
+		return '/' + this.name;
+	}
 }
 
 class App extends React.Component  {
 
-   constructor(props) {
-      super(props);
-      Object.assign(this, SocketMixin);
-      AppConfig.socket = IO();
-   }
+	constructor(props) {
+		super(props);
+		Object.assign(this, SocketMixin);
+		AppConfig.socket = IO();
+	}
 
-   get games() {
-      return [
-        new Game({
-          name: 'Boggle',
-          getGameComponent: (props) => <Boggle {...props}/>,
-          initialState: 'room'
-        }),
-        new Game({
-          name: 'Pong',
-          getGameComponent: (props) => <Pong {...props}/>,
-          initialState: 'room'
-        }),
-        new Game({
-          name: 'Trivia',
-          getPath: function() { return '/' + this.name; },
-        }),
-        new Game({
-          name: 'Hippos',
-          getPath: function() { return '/' + this.name; },
-        })
-      ];
-   }
+	get games() {
+		return [
+			new Game({
+				name: 'Boggle',
+				getGameComponent: (props) => <Boggle {...props}/>,
+				initialState: 'room'
+			}),
+			new Game({
+				name: 'Pong',
+				getGameComponent: (props) => <Pong {...props}/>,
+				initialState: 'room'
+			}),
+			new Game({
+				name: 'Trivia'
+			}),
+			new Game({
+				name: 'Hippos'
+			})
+		];
+	}
 
-   getGameMenu() {
-      return <GameMenu games={this.games} />;
-   }
+	getGameMenu() {
+		return <GameMenu games={this.games} />;
+	}
 
-   render() {
-      return (
-         <HashRouter>
-           <Switch>
-             <Route exact path='/' component={this.getGameMenu.bind(this)}/>
-             <Route exact path='/room' component={Room} />
-             <Route exact path='/waiting' component={Waiting} />
-             <Route exact path='/results' component={Results} />
-             {this.games.map(game => {
-               return <Route key={game.name} path={game.getPath()} component={game.getGameComponent} />
-             })}
-           </Switch>
-         </HashRouter>          
-      )
-   }
-
+	render() {
+		return (
+			<HashRouter>
+				<Switch>
+					<Route exact path='/' component={this.getGameMenu.bind(this)}/>
+					<Route exact path='/room' component={Room} />
+					<Route exact path='/waiting' component={Waiting} />
+					<Route exact path='/results' component={Results} />
+					{this.games.map(game => {
+						return <Route key={game.name} path={game.getPath()} component={game.getGameComponent} />
+					})}
+				</Switch>
+			</HashRouter>          
+		)
+	}
 }
 
 const ComingSoon = () => {
-   return <div>Coming soon</div>;
+	return <div>Coming soon</div>;
 }
 
 export default App;
