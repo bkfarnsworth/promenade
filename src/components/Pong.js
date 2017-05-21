93057e696729fd42446ef4c8a9e1fd2136c49e9f
Brian Farnsworth <farnswor@adobe.com>
2017-05-21 11:43:47 -0600

    Got basic key handling working for pong

--- src/components/Pong.js
+++ src/components/Pong.js
@@ -1,10 +1,41 @@
 import React from 'react';
+import $ from 'jquery';
+import KeyCodes from './../KeyCodes'
+
 
 class Pong extends React.Component  {
 
+   componentDidMount() {
+
+      $(window).on('keydown', (e) => {
+
+         let $div = $('#pong-div')
+
+         if(KeyCodes.fromEvent(e) === 'UP_ARROW') {
+            $div.css('marginTop', (i, v) => {
+               return (parseFloat(v) - 10) + 'px';
+            })
+         }else if(KeyCodes.fromEvent(e) === 'LEFT_ARROW') {
+            $div.css('marginLeft', (i, v) => {
+               return (parseFloat(v) - 10) + 'px';
+            })
+         }else if(KeyCodes.fromEvent(e) === 'RIGHT_ARROW') {
+            $div.css('marginLeft', (i, v) => {
+               return (parseFloat(v) + 10) + 'px';
+            })
+         }else if(KeyCodes.fromEvent(e) === 'DOWN_ARROW') {
+            $div.css('marginTop', (i, v) => {
+               return (parseFloat(v) + 10) + 'px';
+            })
+         }
+      });
+   }
+
+
+
 	render() {
 		return (
-			<div>
+			<div id="pong-div">
 				Pong
 			</div>
 		)
