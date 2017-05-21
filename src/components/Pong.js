47a5117eadb2480d71eeec92726b461f88bf480e
Brian Farnsworth <farnswor@adobe.com>
2017-05-21 11:56:03 -0600

    A bit more pong

--- src/components/Pong.js
+++ src/components/Pong.js
@@ -9,35 +9,59 @@ class Pong extends React.Component  {
 
       $(window).on('keydown', (e) => {
 
-         let $div = $('#pong-div')
+         let $div = $('#pong-current-player')
 
          if(KeyCodes.fromEvent(e) === 'UP_ARROW') {
-            $div.css('marginTop', (i, v) => {
+            $div.css('top', (i, v) => {
                return (parseFloat(v) - 10) + 'px';
             })
          }else if(KeyCodes.fromEvent(e) === 'LEFT_ARROW') {
-            $div.css('marginLeft', (i, v) => {
+            $div.css('left', (i, v) => {
                return (parseFloat(v) - 10) + 'px';
             })
          }else if(KeyCodes.fromEvent(e) === 'RIGHT_ARROW') {
-            $div.css('marginLeft', (i, v) => {
+            $div.css('left', (i, v) => {
                return (parseFloat(v) + 10) + 'px';
             })
          }else if(KeyCodes.fromEvent(e) === 'DOWN_ARROW') {
-            $div.css('marginTop', (i, v) => {
+            $div.css('top', (i, v) => {
                return (parseFloat(v) + 10) + 'px';
             })
          }
+
+         //emit an update to the room
+
       });
+
+      //socket.on update
+         //move the other player
+
+
    }
 
 
 
 	render() {
+
+      let p1Styles = {
+         position: 'absolute',
+         top: 0
+      }
+
+      let p2Styles = {
+         position: 'absolute',
+         top: '20px'
+      }
+
 		return (
-			<div id="pong-div">
-				Pong
+         <div>
+			<div id="pong-current-player" style={p1Styles}>
+				Me
 			</div>
+         <div id="pong-player-2" style={p2Styles}>
+            Other Player
+         </div>
+         </div>
 		)
 	}
 };
