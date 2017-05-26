eafcf1040e94b287fa05c602a605a5b62266b440
Brian Farnsworth <farnswor@adobe.com>
2017-05-26 00:05:09 -0600

    Commenting out phaser for now because it is huge

--- src/components/Pong.js
+++ src/components/Pong.js
@@ -2,31 +2,31 @@ import React from 'react';
 import $ from 'jquery';
 import KeyCodes from './../KeyCodes'
 import SocketMixin from './SocketMixin';
-import PhaserPong from './PhaserPong';
+// import PhaserPong from './PhaserPong';
 import './Pong.css';
 
 
 
 class Pong extends React.Component  {
 
-   constructor(props) {
-      super(props)
-      Object.assign(this, SocketMixin);
-      Object.assign(this, props);
-      Object.assign(this, _.get(props, 'location.state', {}));
-   }
+   // constructor(props) {
+   //    super(props)
+   //    Object.assign(this, SocketMixin);
+   //    Object.assign(this, props);
+   //    Object.assign(this, _.get(props, 'location.state', {}));
+   // }
 
-   componentDidMount() {
-   	let pong = new PhaserPong(this.socket, {
-         hosting: this.playerType === 'host', 
-         playersCount: this.players.length,
-         playerNum: this.players.indexOf(this.userName)
-      });
-   }
+   // componentDidMount() {
+   // 	let pong = new PhaserPong(this.socket, {
+   //       hosting: this.playerType === 'host', 
+   //       playersCount: this.players.length,
+   //       playerNum: this.players.indexOf(this.userName)
+   //    });
+   // }
 
-   componentWillUnmount() {
-      this.callOffFuncs();
-   }
+   // componentWillUnmount() {
+   //    this.callOffFuncs();
+   // }
 
 	render() {
 		return (
