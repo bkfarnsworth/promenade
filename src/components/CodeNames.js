496385651bb5b9aefef9322c93aa879b4b1db03e
Brian Farnsworth <farnswor@adobe.com>
2017-12-22 00:59:53 -0700

    got the board working

--- src/components/CodeNames.js
+++ src/components/CodeNames.js
@@ -6,6 +6,7 @@ import DebugHelper from './../DebugHelper';
 // import './Boggle.css';
 import _ from 'lodash';
 // import $ from 'jquery';
+import Grid from './Grid';
 
 class CodeNames extends React.Component  {
 
@@ -15,19 +16,62 @@ class CodeNames extends React.Component  {
       Object.assign(this, props);
       Object.assign(this, _.get(props, 'location.state', {}));
       Object.assign(this, _.get(props, 'location.state.gameProps', {}));
+      this.state = {
+         needsConfig: true
+      }
    }
 
    componentWillUnmount() {
       this.callOffFuncs();
    }
 
+   // getConfigComponent() {
+   //    return () => <div>config</div>;      
+   // }
+
    render() {
-      return (
-         <div>
-            Code Names
-         </div>
-      )
+
+      // let {
+      //    needsConfig
+      // } = this.state;
+
+      // let Config = this.getConfigComponent();
+      // let Board = this.getBoardComponent();
+      // let BoardMap = this.getBoardMapComponent();
+
+      // if(needsConfig) {
+      //    return <Config playerType={this.playerType}/>
+      // } else {
+
+       // {hintGiver ? <BoardMap/> : null}
+
+         return (
+            <div>
+               <Board/>
+            </div>
+         )
+      // }
    }
 };
+
+const CellContents = (props) => {
+   let {cell} = props;
+   return <div>{cell.name}</div>;
+}
+
+const Board = (props) => {
+
+   let board = {
+      rows: [
+         {cells: [{name: 'noun'}, {name: 'noun'}, {name: 'noun'}, {name: 'noun'}]},
+         {cells: [{name: 'noun'}, {name: 'noun'}, {name: 'noun'}, {name: 'noun'}]},
+         {cells: [{name: 'noun'}, {name: 'noun'}, {name: 'noun'}, {name: 'noun'}]},
+         {cells: [{name: 'noun'}, {name: 'noun'}, {name: 'noun'}, {name: 'noun'}]},
+         {cells: [{name: 'noun'}, {name: 'noun'}, {name: 'noun'}, {name: 'noun'}]},
+      ]
+   }
+
+   return <Grid grid={board} cellContents={CellContents}/>
+}
 
 export default CodeNames;
